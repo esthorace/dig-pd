d8663a838cc7f944a80e487b3f74140e25c45f3e
Esteban <esteban.horacio.acevedo@gmail.com>
2026-08-19 21:57:18 -0300

    Consigna ejercicio 2

---
+++ clase1/2_ejercicio.py
@@ -0,0 +1,22 @@
+"""
+Una empresa debe aprobar o no un crédito para un cliente.
+Las condiciones son las siguientes:
+    - El cliente debe ser mayor de edad.
+    - Debe tener una antigüedad en el sistema financiero mínimo de 3 años
+    y un ingreso mayor a 2500 dólares.
+    - En caso de que no tenga la antigüedad suficiente,
+    su ingreso mensual debe ser como mínimo 4000 dólares.
+Si no cumple ninguna de las condiciones, no se aprueba el crédito.
+
+-> No usar funciones, ni try-except, ni POO
+"""
+
+# Entrada de datos
+edad = int(input("Ingrese su edad: "))
+antiguedad = int(input("Ingrese su antigüedad en el sistema financiero: "))
+ingreso = float(input("Ingrese su ingreso mensual: "))
+
+# Validación
+
+
+# Salida
