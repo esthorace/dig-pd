e76a18d44c2b576efe8baa40613716d605faf3bc
Esteban <esteban.horacio.acevedo@gmail.com>
2026-08-19 21:36:57 -0300

    Agrega consigna

---
+++ clase1/1_ejercicio.py
@@ -0,0 +1,12 @@
+"""
+A partir de dos variables llamadas nombre y edad:
+crear una variable que almacene si se cumplen las siguientes condiciones,
+y mostrar al usuario True o False:
+    - nombre sea diferente de cuatro asteriscos ****
+    - edad sea mayor que 5 y a su vez menor que 20
+    - Que la longitud de nombre sea mayor o igual a 4 pero a la vez menor que 8
+    - edad multiplicada por 3 sea mayor que 35
+
+No debes usar funciones, ni condicionales (if), bucles (while-for) o cualquier
+otra instrucción que no hayamos visto.
+"""
